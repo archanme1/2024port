3d4bd107665272b8442313a73cc16405cfe44a7e
Archan Bhatta <archanme1@gmail.com>
2024-09-11 18:48:32 -0400

    location

--- components/CodeDisplay.js
+++ components/CodeDisplay.js
@@ -16,22 +16,30 @@ const CodeDisplay = () => {
         4&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; const [interests, setInterests] =
         useState(&apos;&apos;);
       </pre>
-      <pre>6</pre>
-      <pre>5&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; useEffect(() =&gt; &#123;</pre>
       <pre>
-        6&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; setName(&quot;
+        5&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; const [location, setLocation] =
+        useState(&apos;&apos;);
+      </pre>
+      <pre>6</pre>
+      <pre>7&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; useEffect(() =&gt; &#123;</pre>
+      <pre>
+        8&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; setName(&quot;
         <b>Archan Bhatta</b>&quot;);
       </pre>
       <pre>
-        7&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; setRole(&quot;
+        9&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; setRole(&quot;
         <b>Full Stack Developer</b>&quot;);
       </pre>
       <pre>
-        8&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; setInterest(&quot;
+        11&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; setLocation(&quot;
+        <b>Ontario, Canada</b>&quot;);
+      </pre>
+      <pre>
+        10&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; setInterests(&quot;
         <b>Web Dev, Mobile App Dev, DevOps, Sports</b>&quot;);
       </pre>
-      <pre>9&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &#125;, []);</pre>
-      <pre>10 &#125;</pre>
+      <pre>12&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &#125;, []);</pre>
+      <pre>13 &#125;</pre>
     </section>
   );
 };
